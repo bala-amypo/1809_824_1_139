b1663957352a37d3e8d6e14004bb5eadcbf2ca18
bala-amypo <bala@amypo.in>
2025-12-16 15:17:01 +0000

    User commit

--- src/main/java/com/example/demo/entity/University.java
+++ src/main/java/com/example/demo/entity/University.java
@@ -1,3 +1,23 @@
+package com.example.demo.entity;
 public class University{
+    private Long id;
+    private String name;
+    private String accreditationLevel;
+    private String country;
+    private Boolean active;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public string getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
     
 }
