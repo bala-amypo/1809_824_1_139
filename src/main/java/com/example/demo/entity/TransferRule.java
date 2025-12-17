cef4234dd383f165033887e7ea3c0da981fca60e
bala-amypo <bala@amypo.in>
2025-12-17 04:03:56 +0000

    User commit

--- src/main/java/com/example/demo/entity/TransferRule.java
+++ src/main/java/com/example/demo/entity/TransferRule.java
@@ -1,7 +1,10 @@
 package com.example.demo.entity;
 public class TransferRule{
     private Long id;
-    private 
-    private 
+    private University sourceUniversity;
+    private University targetUniversity;
+    private Double minimumOverlapPercentage;
+    private Integer creditHourTolerance;
+    private Boolean active;
     
 }
