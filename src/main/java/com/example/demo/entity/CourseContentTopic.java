a5613c448885c3e22c01c203ad311ee3c35f130f
bala-amypo <bala@amypo.in>
2025-12-18 06:32:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/CourseContentTopic.java
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -1,40 +1,44 @@
-// package com.example.demo.entity;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// @Entity
-// public class CourseContentTopic{
-//     @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Long id;
-//     private Course course;
-//     private String topicName;
-//     private Double weightPercentage;
-
-//     public Long getid(){
-//         return id;
-//     }
-//     public void setid(Long id){
-//         this.id=id;
-//     }
-//      public Course getcourse(){
-//         return course;
-//     }
-//     public void setcourse(Course course){
-//         this.course=course;
-//     }
-//     public String gettopicName(){
-//         return topicName;
-//     }
-//     public void settopicName(String topicName){
-//         this.topicName=topicName;
-//     }
-//     public Double getweightPercentage(){
-//         return weightPercentage;
-//     }
-//     public void setweightPercentage(Double weightPercentage){
-//         this.weightPercentage=weightPercentage;
-//     }
+package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+@Entity
+public class CourseContentTopic{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
     
-// }
+    private String topicName;
+    private Double weightPercentage;
+
+    public Long getid(){
+        return id;
+    }
+    public void setid(Long id){
+        this.id=id;
+    }
+    
+    public String gettopicName(){
+        return topicName;
+    }
+    public void settopicName(String topicName){
+        this.topicName=topicName;
+    }
+    public Double getweightPercentage(){
+        return weightPercentage;
+    }
+    public void setweightPercentage(Double weightPercentage){
+        this.weightPercentage=weightPercentage;
+    }
+    public CourseContentTopic(Long id,String topicName,Double weightPercentage){
+        this.id=id;
+        this.topicName=topicName;
+        this.weightPercentage=weightPercentage;
+    }
+    public CourseContentTopic(){
+        
+    }
+
+    
+}
--- src/main/java/com/example/demo/entity/TransferEvaluationResult.java
+++ src/main/java/com/example/demo/entity/TransferEvaluationResult.java
@@ -1,69 +1,69 @@
-package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+// package com.example.demo.entity;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
 
-@Entity
-public class TransferEvaluationResult{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    private Double overlapPercentage;
-    private Integer creditHourDifference;
-    private Boolean isEligibleForTransfer;
-    private String notes;
+// @Entity
+// public class TransferEvaluationResult{
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.IDENTITY)
+//     private Long id;
+//     private Double overlapPercentage;
+//     private Integer creditHourDifference;
+//     private Boolean isEligibleForTransfer;
+//     private String notes;
 
-    public Long getid(){
-        return id;
-    }
-    public void setid(Long id){
-        this.id=id;
-    }
+//     public Long getid(){
+//         return id;
+//     }
+//     public void setid(Long id){
+//         this.id=id;
+//     }
    
     
-    public Double getoverlapPercentage(){
-        return overlapPercentage;
-    }
-    public void setoverlapPercentage(Double overlapPercentage){
-        this.overlapPercentage=overlapPercentage;
-    }
+//     public Double getoverlapPercentage(){
+//         return overlapPercentage;
+//     }
+//     public void setoverlapPercentage(Double overlapPercentage){
+//         this.overlapPercentage=overlapPercentage;
+//     }
 
-    public Integer getcreditHourDifference(){
-        return creditHourDifference;
-    }
-    public void setcreditHourDifference(Integer creditHourDifference){
-        this.creditHourDifference=creditHourDifference;
-    }
+//     public Integer getcreditHourDifference(){
+//         return creditHourDifference;
+//     }
+//     public void setcreditHourDifference(Integer creditHourDifference){
+//         this.creditHourDifference=creditHourDifference;
+//     }
 
-     public Boolean getisEligibleForTransfer(){
-        return isEligibleForTransfer;
-    }
-    public void setisEligibleForTransfer(Boolean isEligibleForTransfer){
-        this.isEligibleForTransfer=isEligibleForTransfer;
-    }
+//      public Boolean getisEligibleForTransfer(){
+//         return isEligibleForTransfer;
+//     }
+//     public void setisEligibleForTransfer(Boolean isEligibleForTransfer){
+//         this.isEligibleForTransfer=isEligibleForTransfer;
+//     }
     
 
-     public String getnotes(){
-        return notes;
-    }
-    public void setnotes(String notes){
-        this.notes=notes;
-    }
+//      public String getnotes(){
+//         return notes;
+//     }
+//     public void setnotes(String notes){
+//         this.notes=notes;
+//     }
 
-    public TransferEvaluationResult(Long id,Double overlapPercentage,Integer creditHourDifference,Boolean isEligibleForTransfer,String notes){
-        this.id=id;
+//     public TransferEvaluationResult(Long id,Double overlapPercentage,Integer creditHourDifference,Boolean isEligibleForTransfer,String notes){
+//         this.id=id;
         
-        this.overlapPercentage=overlapPercentage;
-        this.creditHourDifference=creditHourDifference;
-        this.isEligibleForTransfer=isEligibleForTransfer;
+//         this.overlapPercentage=overlapPercentage;
+//         this.creditHourDifference=creditHourDifference;
+//         this.isEligibleForTransfer=isEligibleForTransfer;
         
-        this.notes=notes;
-    }
-    public TransferEvaluationResult(){
+//         this.notes=notes;
+//     }
+//     public TransferEvaluationResult(){
 
-    }
-    }
+//     }
+//     }
     
     
     
