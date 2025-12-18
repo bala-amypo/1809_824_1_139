e3548942b059d7699d25a85b89e5f4bdb43c04e2
bala-amypo <bala@amypo.in>
2025-12-18 15:35:43 +0000

    User commit

---
+++ src/main/java/service/impl/UniversityServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.service.UniversityService;
+import com.example.demo.entity.University;
+import com.example.demo.repository.UniversityRepository;
+
+import java.util.List;
+
