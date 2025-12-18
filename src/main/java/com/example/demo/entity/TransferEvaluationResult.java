6668fd9af868ec68c94dedb283336793f12ef683
bala-amypo <bala@amypo.in>
2025-12-18 06:22:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/TransferEvaluationResult.java
+++ src/main/java/com/example/demo/entity/TransferEvaluationResult.java
@@ -3,17 +3,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import java.sql.Timestamp;
+
 @Entity
 public class TransferEvaluationResult{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-   
     private Double overlapPercentage;
     private Integer creditHourDifference;
-    private Boolean isEligibleForTransfer;
-    private Timestamp evaluatedAt;
     private String notes;
 
     public Long getid(){
@@ -30,24 +27,21 @@ public class TransferEvaluationResult{
     public void setoverlapPercentage(Double overlapPercentage){
         this.overlapPercentage=overlapPercentage;
     }
+
     public Integer getcreditHourDifference(){
         return creditHourDifference;
     }
     public void setcreditHourDifference(Integer creditHourDifference){
         this.creditHourDifference=creditHourDifference;
     }
+
      public Boolean getisEligibleForTransfer(){
         return isEligibleForTransfer;
     }
     public void setisEligibleForTransfer(Boolean isEligibleForTransfer){
         this.isEligibleForTransfer=isEligibleForTransfer;
     }
-     public Timestamp getevaluatedAt(){
-        return evaluatedAt;
-    }
-    public void setEvaluatedAt(Timestamp evaluatedAt){
-        this.evaluatedAt=evaluatedAt;
-    }
+    
 
      public String getnotes(){
         return notes;
@@ -55,19 +49,22 @@ public class TransferEvaluationResult{
     public void setnotes(String notes){
         this.notes=notes;
     }
-    public TransferEvaluationResult(Long id,Double overlapPercentage,Integer creditHourDifference,Boolean isEligibleTransfer,Timestamp evaluatedAt,String notes){
+    
+    public TransferEvaluationResult(Long id,Double overlapPercentage,Integer creditHourDifference,Boolean isEligibleTransfer,String notes){
         this.id=id;
         
         this.overlapPercentage=overlapPercentage;
         this.creditHourDifference=creditHourDifference;
         this.isEligibleForTransfer=isEligibleForTransfer;
-        this.evaluatedAt=evaluatedAt;
+        
         this.notes=notes;
     }
     public TransferEvaluationResult(){
 
     }
     }
+    
+    
     
     
 
