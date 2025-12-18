f5fc182c4248885de9f7e91b2505cfcfe061925b
bala-amypo <bala@amypo.in>
2025-12-18 05:22:10 +0000

    User commit

--- src/main/java/com/example/demo/repository/CourseContentTopicRepository.java
+++ src/main/java/com/example/demo/repository/CourseContentTopicRepository.java
@@ -2,5 +2,5 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.CourseContentTopic;
 public interface CourseContentTopicRepository extends JpaRepository<CourseContentTopic,Long>{
-    
+
 }
--- src/main/java/service/AuthService.java
+++
@@ -1,5 +0,0 @@
-// import com.example.demo.entity.Auth;
-// public interface AuthService{
-//     register(voa Controller/Service logic);
-//     login(via Controller/Service logic);
-// }
