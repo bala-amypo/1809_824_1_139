9a8651008ab5e0429e0f0e51722d92bd729d6afc
bala-amypo <bala@amypo.in>
2025-12-18 05:57:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,62 +1,62 @@
-package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import java.time.LocalDateTime;
-@Entity
-public class User{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    @Column(unique=true)
-    private String email;
-    private String password;
-    private String roles;
-    private LocalDateTime createdAt;
+// package com.example.demo.entity;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Column;
+// import java.time.LocalDateTime;
+// @Entity
+// public class User{
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.IDENTITY)
+//     private Long id;
+//     @Column(unique=true)
+//     private String email;
+//     private String password;
+//     private String roles;
+//     private LocalDateTime createdAt;
 
-     public Long getid(){
-        return id;
-    }
-    public void setid(Long id){
-        this.id=id;
-    }
-     public String getemail(){
-        return email;
-    }
-    public void setemail(String email){
-        this.email=email;
-    }
-     public String getpassword(){
-        return password;
-    }
-    public void setpassword(String password){
-        this.password=password;
-    }
-     public String getroles(){
-        return roles;
-    }
-    public void setroles(String roles){
-        this.roles=roles;
-    }
-     public LocalDateTime getcreatedAt(){
-        return createdAt;
-    }
-    public void setcreatedAt(LocalDateTime createdAt){
-        this.createdAt=createdAt;
-    }
-    public User(Long id,String email,String password,String roles,LocalDateTime createdAt){
-        this.id=id;
-        this.email=email;
-        this.password=password;
-        this.roles=roles;
-        this.createdAt=createdAt;
-    }
-    public User(){
+//      public Long getid(){
+//         return id;
+//     }
+//     public void setid(Long id){
+//         this.id=id;
+//     }
+//      public String getemail(){
+//         return email;
+//     }
+//     public void setemail(String email){
+//         this.email=email;
+//     }
+//      public String getpassword(){
+//         return password;
+//     }
+//     public void setpassword(String password){
+//         this.password=password;
+//     }
+//      public String getroles(){
+//         return roles;
+//     }
+//     public void setroles(String roles){
+//         this.roles=roles;
+//     }
+//      public LocalDateTime getcreatedAt(){
+//         return createdAt;
+//     }
+//     public void setcreatedAt(LocalDateTime createdAt){
+//         this.createdAt=createdAt;
+//     }
+//     public User(Long id,String email,String password,String roles,LocalDateTime createdAt){
+//         this.id=id;
+//         this.email=email;
+//         this.password=password;
+//         this.roles=roles;
+//         this.createdAt=createdAt;
+//     }
+//     public User(){
         
-    }
-    }
+//     }
+//     }
 
 
     
