ac29bdbcad15fb5052068ec574791324eec3b514
bala-amypo <bala@amypo.in>
2025-12-18 03:42:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/CourseContentTopic.java
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -1,40 +1,40 @@
-package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-@Entity
-public class CourseContentTopic{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    private Course course;
-    private String topicName;
-    private Double weightPercentage;
+// package com.example.demo.entity;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// @Entity
+// public class CourseContentTopic{
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.IDENTITY)
+//     private Long id;
+//     private Course course;
+//     private String topicName;
+//     private Double weightPercentage;
 
-    public Long getid(){
-        return id;
-    }
-    public void setid(Long id){
-        this.id=id;
-    }
-     public Course getcourse(){
-        return course;
-    }
-    public void setcourse(Course course){
-        this.course=course;
-    }
-    public String gettopicName(){
-        return topicName;
-    }
-    public void settopicName(String topicName){
-        this.topicName=topicName;
-    }
-    public Double getweightPercentage(){
-        return weightPercentage;
-    }
-    public void setweightPercentage(Double weightPercentage){
-        this.weightPercentage=weightPercentage;
-    }
+//     public Long getid(){
+//         return id;
+//     }
+//     public void setid(Long id){
+//         this.id=id;
+//     }
+//      public Course getcourse(){
+//         return course;
+//     }
+//     public void setcourse(Course course){
+//         this.course=course;
+//     }
+//     public String gettopicName(){
+//         return topicName;
+//     }
+//     public void settopicName(String topicName){
+//         this.topicName=topicName;
+//     }
+//     public Double getweightPercentage(){
+//         return weightPercentage;
+//     }
+//     public void setweightPercentage(Double weightPercentage){
+//         this.weightPercentage=weightPercentage;
+//     }
     
-}
+// }
