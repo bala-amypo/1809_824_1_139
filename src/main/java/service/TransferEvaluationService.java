9dcfd4c6d2c5b881dd58a0e9ed0eddab72962e92
bala-amypo <bala@amypo.in>
2025-12-18 16:26:43 +0000

    User commit

--- src/main/java/service/TransferEvaluationService.java
+++ src/main/java/service/TransferEvaluationService.java
@@ -1,7 +1,8 @@
 package com.example.demo.service;
-import com.example.demo.entity.TranferEvaluation;
-public interface TransferEvaluationService{
-    TransferEvaluation evaluate Transfer(Long sourceCourseId,Long targetCourseId);
+import com.example.demo.entity.TransferEvaluation;
+import java.util.List;
+public interface TransferEvaluationService {
+    TransferEvaluation evaluateTransfer(Long sourceCourseId, Long targetCourseId);
     TransferEvaluation getEvaluationById(Long id);
-    List<TransferEvaluation>getEvaluationForCourse(Long courseId);
+    List<TransferEvaluation> getEvaluationForCourse(Long courseId);
 }
