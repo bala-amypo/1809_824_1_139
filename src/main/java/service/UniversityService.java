9346e31391b6bee3758bbb6d388953bb82960082
bala-amypo <bala@amypo.in>
2025-12-18 08:22:22 +0000

    User commit

--- src/main/java/service/UniversityService.java
+++ src/main/java/service/UniversityService.java
@@ -1,3 +1,9 @@
 package.com.example.demo.service;
-import com.example.demo.entity.UniversityService;
-public interface
+import com.example.demo.entity.University;
+i p
+public interface UniversityService{
+    createUniversity(University univ);
+    updateUniversity(Long id,University univ);
+    getUniversityById(Long id);
+    List<University>getAllUniversities();
+}
