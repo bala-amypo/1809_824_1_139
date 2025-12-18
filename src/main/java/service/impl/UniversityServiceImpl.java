821fc3b0920867f67264e587881c1737377179fe
bala-amypo <bala@amypo.in>
2025-12-18 15:37:23 +0000

    User commit

--- src/main/java/service/impl/UniversityServiceImpl.java
+++ src/main/java/service/impl/UniversityServiceImpl.java
@@ -7,3 +7,38 @@ import com.example.demo.repository.UniversityRepository;
 
 import java.util.List;
 
+@Service
+public class UniversityServiceImpl implements UniversityService {
+
+    private final UniversityRepository repo;
+
+    public UniversityServiceImpl(UniversityRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public University createUniversity(University university) {
+        return repo.save(university);
+    }
+
+    @Override
+    public University updateUniversity(Long id, University university) {
+        university.setId(id);
+        return repo.save(university);
+    }
+
+    @Override
+    public University getUniversityById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<University> getAllUniversities() {
+        return repo.findAll();
+    }
+
+    @Override
+    public void deactivateUniversity(Long id) {
+        repo.deleteById(id);   
+    }
+}
