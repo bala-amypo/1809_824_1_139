cd17c2a3cd3ab7ff54666814c8cc9e3abb6400fd
bala-amypo <bala@amypo.in>
2025-12-18 16:05:43 +0000

    User commit

--- src/main/java/service/TransferEvaluationService.java
+++ src/main/java/service/TransferEvaluationService.java
@@ -1,2 +1,7 @@
 package com.example.demo.service;
-import com.example.demo.entity.TranferRule
+import com.example.demo.entity.TranferEvaluation;
+public interface TransferEvaluationService{
+    evaluate Transfer(Long sourceCourseId,Long targetCourseId);
+    getEvaluationById(Long id);
+    getEvaluationForCourse(Long courseId);
+}
