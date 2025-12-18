5d6366a6ed3d84dc076c1e881c0372c4ae957e1f
bala-amypo <bala@amypo.in>
2025-12-18 10:18:23 +0000

    User commit

--- src/main/java/com/example/demo/repository/TransferRuleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.demo.entity.TransferRule;
-import java.util.Long;
-@Repository
-public interface TransferRuleRepository extends JpaRepository<TransferRule, Long> {
-    }
