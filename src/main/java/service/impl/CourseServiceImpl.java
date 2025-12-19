a794d6d65f2135452c2ad14f5d7f745482396838
bala-amypo <bala@amypo.in>
2025-12-19 08:39:50 +0000

    User commit

---
+++ src/main/java/service/impl/CourseServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.service.CourseService;
+import com.example.demo.entity.Course;
+import com.example.demo.repository.CourseRepository;
+
+import java.util.List;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+    private final CourseRepository repo;
+
+    public CourseServiceImpl(CourseRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public Course createCourse(Course course) {
+        return repo.save(course);
+    }
+
+    @Override
+    public Course updateCourse(Long id, Course course) {
+        course.setId(id);
+        return repo.save(course);
+    }
+
+    @Override
+    public Course getCourseById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+    @Override
+    public Course getCourseByUniversity(Long universityId){
+        return repo.findByUniversityId(id).orElse(null);
+    }
+
+    
+
+    @Override
+    public Course deactivateCourse(Long id) {
+        repo.deleteById(id);   
+    }
+}
