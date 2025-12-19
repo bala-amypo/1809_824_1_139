4254d866a18f457f9fa7292c2a301284a3a60b26
bala-amypo <bala@amypo.in>
2025-12-19 03:35:55 +0000

    User commit

--- src/main/java/service/TransferEvaluationService.java
+++ src/main/java/service/TransferEvaluationService.java
@@ -1,8 +1,8 @@
-// package com.example.demo.service;
-// import com.example.demo.entity.TransferEvaluation;
-// import java.util.List;
-// public interface TransferEvaluationService {
-//     TransferEvaluation evaluateTransfer(Long sourceCourseId, Long targetCourseId);
-//     TransferEvaluation getEvaluationById(Long id);
-//     List<TransferEvaluation> getEvaluationForCourse(Long courseId);
-// }
+//package com.example.demo.service;
+//import com.example.demo.entity.TransferEvaluation;
+//import java.util.List;
+//public interface TransferEvaluationService {
+  //  TransferEvaluation evaluateTransfer(Long sourceCourseId, Long targetCourseId);
+    //TransferEvaluation getEvaluationById(Long id);
+    //List<TransferEvaluation> getEvaluationForCourse(Long courseId);
+//}
--- src/main/java/service/TransferRuleService.java
+++ src/main/java/service/TransferRuleService.java
@@ -1,10 +1,9 @@
-package com.example.demo.service;
-import com.example.demo.entity.TransferRule;
-public interface TransferRuleService{
-    TransferRule createRule(TransferRule rule);
-    void updateRule(Long id,TransferRule rule);
-    TransferRule getRuleById(Long id);
-    List<TranferRule>getRulesForUniversities(Long sourceId,Long targetId);
-    void deactivateRule(Long id);
-
-}
+// package com.example.demo.service;
+// import com.example.demo.entity.TransferRule;
+// public interface TransferRuleService{
+  //  TransferRule createRule(TransferRule rule);
+  //  void updateRule(Long id,TransferRule rule);
+   // TransferRule getRuleById(Long id);
+   // List<TranferRule>getRulesForUniversities(Long sourceId,Long targetId);
+   // void deactivateRule(Long id);
+//}
