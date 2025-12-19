e2df35783c6964879365dcd81e0b215fbae62a1d
bala-amypo <bala@amypo.in>
2025-12-19 06:13:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -1,14 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.entity.Course;
 import com.example.demo.service.CourseService;
@@ -16,16 +10,19 @@ import com.example.demo.service.CourseService;
 import java.util.List;
 
 @RestController
+@RequestMapping("/courses")
 public class CourseController {
 
     @Autowired
     CourseService uni;   
 
+    // POST /
     @PostMapping("/")
     public Course addCourse(@RequestBody Course course) {
         return uni.createCourse(course);
     }
 
+    // PUT /{id}
     @PutMapping("/{id}")
     public Course updatecourse(
             @PathVariable Long id,
@@ -33,22 +30,27 @@ public class CourseController {
         return uni.updateCourse(id, course); 
     }
 
+    // GET /{id}
     @GetMapping("/{id}")
     public Course getcourse(@PathVariable Long id) {
         return uni.getCourseById(id); 
     }
 
-   
+    // GET /
+    @GetMapping("/")
+    public List<Course> getAllCourses() {
+        return uni.getAllCourses();
+    }
 
+    // GET /university/{universityId}
     @GetMapping("/university/{universityId}")
-    public List<Course> getCourseByUniversityId(
-            @PathVariable Long universityId) {
+    public List<Course> getCourseByUniversityId(@PathVariable Long universityId) {
         return uni.getCourseByUniversityId(universityId);
     }
 
+    // PUT /{id}/deactivate
     @PutMapping("/{id}/deactivate")
     public void deactivateCourse(@PathVariable Long id) {
         uni.deactivateCourse(id);
     }
 }
-
