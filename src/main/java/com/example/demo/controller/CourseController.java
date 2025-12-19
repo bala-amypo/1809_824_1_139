34896f8eac0f4056573b8e01a6f540e655d6ef28
bala-amypo <bala@amypo.in>
2025-12-19 05:49:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -10,24 +10,24 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.demo.entity.University;
-import com.example.demo.service.UniversityService;
+import com.example.demo.entity.Course;
+import com.example.demo.service.CourseService;
 
 import java.util.List;
 
 @RestController
-public class UniversityController {
+public class CourseController {
 
     @Autowired
-    UniversityService uni;   
+    CourseService uni;   
 
     @PostMapping("/")
-    public University addUniversity(@RequestBody University university) {
-        return uni.createUniversity(university);
+    public Course addCourse(@RequestBody Course course) {
+        return uni.createCourse(course);
     }
 
     @PutMapping("/{id}")
-    public University updateuniversity(@PathVariable Long id,
+    public Course updatecourse(@PathVariable Long id,
                                        @Valid @RequestBody University university) {
         return uni.updateUniversity(id, university); 
     }
