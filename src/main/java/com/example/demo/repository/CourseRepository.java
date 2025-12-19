f3077048770896e05cbc72c0712f0f12e2c47bf3
bala-amypo <bala@amypo.in>
2025-12-19 15:35:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/CourseRepository.java
+++ src/main/java/com/example/demo/repository/CourseRepository.java
@@ -1,8 +1,8 @@
-//package com.example.demo.repository;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//import com.example.demo.entity.Course;
-//@Repository
-//public interface CourseRepository extends JpaRepository<Course, Long> {
+package com.example.demo.repository;
+ import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+ import com.example.demo.entity.Course;
+ @Repository
+ public interface CourseRepository extends JpaRepository<Course, Long> {
 
-    //}
+     }
--- src/main/java/service/CourseService.java
+++ src/main/java/service/CourseService.java
@@ -1,10 +1,10 @@
-// package com.example.demo.service;
-// import com.example.demo.entity.Course;
-// import java.util.List;
-// public interface CourseService{
-//     Course createCourse(Course course);
-//     Course updateCourse(Long id,Course course);
-//    Course getCourseById(Long id);
-//    List<Course>getCoursesByUniversityId(Long universityId);
-//    Course deactivateCourse(Long id);
-//}
+package com.example.demo.service;
+ import com.example.demo.entity.Course;
+ import java.util.List;
+ public interface CourseService{
+     Course createCourse(Course course);
+     Course updateCourse(Long id,Course course);
+    Course getCourseById(Long id);
+    List<Course>getCoursesByUniversityId(Long universityId);
+    Course deactivateCourse(Long id);
+ }
