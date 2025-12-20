e1eb5b11dc3fd8ad4e0ad5c7384fbb9835a37e4d
bala-amypo <bala@amypo.in>
2025-12-20 07:30:24 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,4 +1,26 @@
 package com.example.demo.security;
-public class JwtTokenProvider{
-    
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Set;
+
+@Component
+public class JwtTokenProvider {
+
+    private final String jwtSecret = "demo-secret-key";
+    private final long jwtExpirationMs = 86400000; // 1 day
+
+    public String generateToken(String email, Set<String> roles) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
 }
