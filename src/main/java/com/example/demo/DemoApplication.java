65041bd76be8da822418b9abf39faa13ed32bb88
bala-amypo <bala@amypo.in>
2025-12-24 08:20:59 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypo.bcryp.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
